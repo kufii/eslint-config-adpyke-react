d8c5831aae07a4f0934228d2e2af0b7aacecfa95
Adrien Pyke <adpyke@gmail.com>
2019-01-22 16:26:53 -0500

    new rules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+	'parser': 'babel-eslint',
 	'extends': ['adpyke-es6', 'plugin:react/recommended'],
 	'parserOptions': {
 		'sourceType': 'module',
@@ -16,6 +17,7 @@ module.exports = {
 		'react'
 	],
 	'rules': {
+		'react/self-closing-comp': 1,
 		'react/jsx-closing-bracket-location': 1,
 		'react/jsx-boolean-value': 1,
 		'react/jsx-closing-tag-location': 1,
@@ -28,6 +30,15 @@ module.exports = {
 		'react/jsx-space-before-closing': 1,
 		'react/jsx-fragments': 2,
 		'react/jsx-pascal-case': 2,
-		'react/jsx-tag-spacing': 2
+		'react/jsx-tag-spacing': 2,
+		'react/forbid-component-props': 2,
+		'react/no-access-state-in-setstate': 2,
+		'react/no-redundant-should-component-update': 2,
+		'react/no-this-in-sfc': 2,
+		'react/no-unused-prop-types': 2,
+		'react/no-unused-state': 2,
+		'react/prefer-es6-class': 2,
+		'react/state-in-constructor': [2, 'never'],
+		'react/style-prop-object': 2
 	}
 };
