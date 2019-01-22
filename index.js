43949d1dd806df16ea7fe9aa32aa29aea0d23e44
Adrien Pyke <adpyke@gmail.com>
2019-01-22 12:13:13 -0500

    update jsx rules

--- index.js
+++ index.js
@@ -14,5 +14,19 @@ module.exports = {
 	},
 	'plugins': [
 		'react'
-	]
+	],
+	'rules': {
+		'react/jsx-closing-bracket-location': 1,
+		'react/jsx-boolean-value': 1,
+		'react/jsx-closing-tag-location': 1,
+		'react/jsx-curly-spacing': 1,
+		'react/jsx-equals-spacing': 1,
+		'react/jsx-first-prop-new-line': 1,
+		'react/jsx-handler-names': 1,
+		'react/jsx-curly-brace-presence': 1,
+		'react/jsx-props-no-multi-spaces': 1,
+		'react/jsx-space-before-closing': 1,
+		'react/jsx-pascal-case': 2,
+		'react/jsx-tag-spacing': 2
+	}
 };
